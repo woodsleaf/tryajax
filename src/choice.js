cf46eee9ca597439fa9ae853dd2b17a230dd8766
imp <woodsleaf@gmail.com>
2019-10-13 03:23:38 +0300

    some fix

--- src/choice.js
+++ src/choice.js
@@ -5,29 +5,29 @@ document.addEventListener(
       //test replace inner
       document.getElementsByClassName('myformajax')[0].textContent ='Hello';
       let html = '';
-      html +=  JSON.stringify(mydata[0]['step1col']);
+      html +=  JSON.stringify(mydata[0].step1col);
       html += '<hr>';
-      html +=  JSON.stringify(mydata[0]['step2row']);
+      html +=  JSON.stringify(mydata[0].step2row);
       html += '<hr>';
-      html +=  JSON.stringify(mydata[0]['datalist']);
+      html +=  JSON.stringify(mydata[0].datalist);
       html += '<hr>';
       html += '<form>';
-      mydata[0]['step1col'].forEach(val => {
+      mydata[0].step1col.forEach(val => {
         html += '<br><input type="radio" name="'  +  val.name  + '" id="' +  val.id  + '">' + '<label for="' + val.id + '">' + val.label + '</label>';
       });
       html += '<br><input type="submit" id="gostep2" value="Go Step 2">';
       html += '</form>';
-      
+
       html += '<hr>';
       html += '<form>';
-      mydata[0]['step2row'].forEach(val => {
+      mydata[0].step2row.forEach(val => {
         html += '<br><input type="radio" name="'  +  val.name  + '" id="' +  val.id  + '">' + '<label for="' + val.id + '">' + val.label + '</label>';
       });
       html += '<br><input type="submit" id="goresult" value="Go Result">';
       html += '</form>';
 
       html += '<hr>';
-      mydata[0]['datalist'].forEach(val => {
+      mydata[0].datalist.forEach(val => {
         html+='<div class="itemshow"><input type="checkbox"><div class="imgwrap">';
         html +=`<img src="${'./content/' + val.url}" alt="${val.name}" title="${val.name}">`;
         html+=`</div><p>${val.name}</p></div>`;
